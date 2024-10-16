46d91ff1c1c1eafc10af9b36d326442fe04ab0fe
Mohammed Eid Abdelmeguid Ibrahim <mohammedeidabdelmeguid@gmail.com>
2024-10-16 10:14:50 +0300

    no need for the folder

--- 1 Login System/register.py
+++ 1 Login System/register.py
@@ -2,8 +2,12 @@ from flask import Blueprint, url_for, render_template, redirect, request
 from flask_login import LoginManager
 from werkzeug.security import generate_password_hash
 import sqlalchemy
-
+import re
 from models import db, Users
+
+def is_valid_email(email):
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(email_regex, email)
 
 register = Blueprint('register', __name__, template_folder='../frontend')
 login_manager = LoginManager()
@@ -18,6 +22,11 @@ def show():
         confirm_password = request.form['confirm-password']
 
         if username and email and password and confirm_password:
+            # Check if email format is valid
+            if not is_valid_email(email):
+                return redirect(url_for('register.show') + '?error=invalid-email')
+
+            # Check if passwords match
             if password == confirm_password:
                 hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
                 try:
@@ -33,6 +42,8 @@ def show():
                     return redirect(url_for('register.show') + '?error=user-or-email-exists')
 
                 return redirect(url_for('login.show') + '?success=account-created')
+            else:
+                return redirect(url_for('register.show') + '?error=passwords-do-not-match')
         else:
             return redirect(url_for('register.show') + '?error=missing-fields')
     else:
